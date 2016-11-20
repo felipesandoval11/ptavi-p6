56e5431bdd80b9aadbcdac4ceea2f9874a156eea
Felipe Sandoval <felipesandovalsibada@outlook.com>
2016-11-20 11:11:24 +0100

    casi terminado

--- server.py
+++ server.py
@@ -10,38 +10,29 @@ import os
 
 class SIPHandler(socketserver.DatagramRequestHandler):
     """Main handler to send a RTP audio stream."""
-    
-    line_number = 0  # for distinct use in ack
-    print("aqui")
-    
+
     def handle(self):
         """Handler to manage incoming users SIP request."""
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            line_str = line.decode('utf-8')
-            print(line_str)
-            if line_str.split(" ")[0] == "INVITE": 
-                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
-                self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            elif line_str.split(" ")[0] == "ACK":
-                if self.line_number == 0:
-                    self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
-                else:
-                    send = "mp32rtp -i 127.0.0.1 -p 23032 < " + sys.argv[3]
-                    os.system(send)
-            elif line_str.split(" ")[0] == "BYE":
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            elif line_str.split(" ")[0] != "":
-                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-            
-            print(line_str)
-            print(self.line_number)
-            self.line_number += 1
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        line = self.rfile.read()
+        line_str = line.decode('utf-8')
+        line_print = line_str.split(" ")
+        if line_str.split(" ")[0] == "INVITE":
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif line_str.split(" ")[0] == "ACK":
+            if len(line_print) != 3:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+            else:
+                #send = "mp32rtp -i 127.0.0.1 -p 23032 < " + sys.argv[3]
+                #os.system(send)
+                self.wfile.write(b"SIP/2.0 DEBERIA ENVIAR RTP\r\n\r\n")
+        elif line_str.split(" ")[0] == "BYE":
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif line_str.split(" ")[0] != "":
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+        print(line_str)
+
 
 if __name__ == "__main__":
     try:
